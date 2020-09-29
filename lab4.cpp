d561ba3d58e68cf381095de0575f763afb03189f
Lina_Li <ilyinadother@gmail.com>
2020-09-29 11:16:14 +0300

    initial commit

---
+++ lab4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int sum(int n);
+
+int main()
+{
+	cout << "Enter n= ";
+	int n2;
+	cin >> n2;
+	cout << "sum(n2)="<<sum(n2)<<endl; 
+	system("pause");
+}
+int sum(int n)
+{
+	if (n == 1)
+		return 2;
+	else
+		return 2 * sum(n / 2) - 1;
+}
